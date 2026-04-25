e2816d6ad2a958a90dda1e45e22160bc73f620c8
PedroXits <pedroo.asn@gmail.com>
2026-04-25 15:26:58 -0300

    Criação dos atributos na entity Review

--- backend/gb/src/main/java/com/gamebox/gb/domain/entities/Review.java
+++ backend/gb/src/main/java/com/gamebox/gb/domain/entities/Review.java
@@ -1,4 +1,37 @@
 package com.gamebox.gb.domain.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private Game game;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
 }

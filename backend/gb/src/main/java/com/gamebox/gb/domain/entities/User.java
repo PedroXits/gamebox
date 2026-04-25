94ba7844b0dd314cb5f6f0c080b092a37cfeb100
PedroXits <pedroo.asn@gmail.com>
2026-04-25 12:21:47 -0300

    Criação do conteúdo de dentro da entity enum

--- backend/gb/src/main/java/com/gamebox/gb/domain/entities/User.java
+++ backend/gb/src/main/java/com/gamebox/gb/domain/entities/User.java
@@ -1,4 +1,34 @@
 package com.gamebox.gb.domain.entities;
 
+import com.gamebox.gb.domain.enums.Role;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "user")
+    private Profile profile;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
---
+++ backend/gb/src/main/java/com/gamebox/gb/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package com.gamebox.gb.domain.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}

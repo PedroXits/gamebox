5644bb3bc773ada0b280e7d369594a39827e44dd
PedroXits <pedroo.asn@gmail.com>
2026-04-25 13:04:32 -0300

    Adição dos atributos na entity Profile

--- backend/gb/src/main/java/com/gamebox/gb/domain/entities/Profile.java
+++ backend/gb/src/main/java/com/gamebox/gb/domain/entities/Profile.java
@@ -1,4 +1,29 @@
 package com.gamebox.gb.domain.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String profileName;
+    private String photo;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "profile")
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "profile")
+    private List<Favorite> favorites;
+
+    @OneToMany(mappedBy = "profile")
+    private List<Wishlist> wishlist;
 }

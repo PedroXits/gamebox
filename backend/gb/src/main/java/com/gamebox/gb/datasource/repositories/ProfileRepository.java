ab25b651cf2d0ca6fac891cff0c723b00035ca10
PedroXits <pedroo.asn@gmail.com>
2026-04-26 14:55:05 -0300

    Criação do ProfileRepository

---
+++ backend/gb/src/main/java/com/gamebox/gb/datasource/repositories/ProfileRepository.java
@@ -0,0 +1,11 @@
+package com.gamebox.gb.datasource.repositories;
+
+import com.gamebox.gb.domain.entities.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByProfileName(String profileName);
+
+}

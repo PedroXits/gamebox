2b3806961fd0f09b7b409a28cc2ef6f0af79e693
PEDRO AUGUSTO DE SANTANA NUNES <pedro.asnunes@senacsp.edu.br>
2026-04-27 20:03:42 -0300

    Adição da função findReviewByGameId no ReviewService

--- backend/gb/src/main/java/com/gamebox/gb/services/ReviewService.java
+++ backend/gb/src/main/java/com/gamebox/gb/services/ReviewService.java
@@ -1,17 +1,22 @@
 package com.gamebox.gb.services;
 
+import com.gamebox.gb.datasource.repositories.GameRepository;
 import com.gamebox.gb.datasource.repositories.PlayedRepository;
 import com.gamebox.gb.datasource.repositories.ReviewRepository;
 import com.gamebox.gb.domain.entities.Review;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReviewService {
 
+    private final GameRepository gameRepository;
     private final ReviewRepository reviewRepository;
     private final PlayedRepository playedRepository;
 
-    public ReviewService(ReviewRepository reviewRepository, PlayedRepository playedRepository) {
+    public ReviewService(GameRepository gameRepository, ReviewRepository reviewRepository, PlayedRepository playedRepository) {
+        this.gameRepository = gameRepository;
         this.reviewRepository = reviewRepository;
         this.playedRepository = playedRepository;
     }
@@ -37,5 +42,18 @@ public class ReviewService {
 
         return reviewRepository.save(review);
     }
+
+    public List<Review> findReviewByGameId(Long gameId) {
+
+        if (!gameRepository.existsById(gameId)) {
+            throw new RuntimeException("Jogo não encontrado");
+        }
+
+        return reviewRepository.findByGameId(gameId);
+    }
+
+
+
+
 
 }

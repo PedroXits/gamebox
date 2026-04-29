24cd13a5b947627c04153d84316d06aaa8d4b3a9
wkarina <karii46y@gmail.com>
2026-04-29 18:29:25 -0300

    movendo colors.ts para constants dentro do src

--- frontend/src/app/index.style.tsx
+++ frontend/src/app/index.style.tsx
@@ -1,4 +1,4 @@
-import { Colors } from "../../constants/Colors"
+import { Colors } from "../constants/Colors"
 import { StyleSheet } from "react-native";
 import { Fonts } from "@/constants/fonts";
 

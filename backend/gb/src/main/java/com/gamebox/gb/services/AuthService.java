718e842ca13dc5657273cfe2c0bfd10ab358a7d4
PedroXits <pedroo.asn@gmail.com>
2026-05-05 17:52:55 -0300

    Criação da função Register no AuthService

--- backend/gb/src/main/java/com/gamebox/gb/services/AuthService.java
+++ backend/gb/src/main/java/com/gamebox/gb/services/AuthService.java
@@ -1,6 +1,10 @@
 package com.gamebox.gb.services;
 
 import com.gamebox.gb.datasource.repositories.UserRepository;
+import com.gamebox.gb.domain.dtos.auth.AuthResponse;
+import com.gamebox.gb.domain.dtos.auth.RegistrRequest;
+import com.gamebox.gb.domain.entities.User;
+import com.gamebox.gb.domain.enums.Role;
 import com.gamebox.gb.security.JwtService;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +19,34 @@ public class AuthService {
         this.jwtService = jwtService;
     }
 
+    public AuthResponse register(RegistrRequest request) {
 
+        userRepository.findByEmail(request.email())
+                .ifPresent(user -> {
+                    throw new RuntimeException("Email já está cadastrado.");
+                });
 
+        User user = new User();
+        user.setEmail(request.email());
+        user.setPassword(request.password());
+        user.setUsername(request.username());
+        user.setRole(Role.USER);
+
+        User savedUser = userRepository.save(user);
+
+        String token = jwtService.generateToken(
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getUsername()
+        );
+
+        return new AuthResponse(
+                token,
+                savedUser.getId(),
+                savedUser.getEmail(),
+                savedUser.getUsername()
+        );
+    }
 
 
 }

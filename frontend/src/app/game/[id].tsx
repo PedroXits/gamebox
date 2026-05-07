a56793d127ac44660f81e7a35e1dcaa0c8299e3c
wkarina <karii46y@gmail.com>
2026-05-06 22:51:45 -0300

    adicionar tela game overview e validar clique por id

--- frontend/src/app/game/[id].tsx
+++ frontend/src/app/game/[id].tsx
@@ -1,1 +1,29 @@
 //game overview dinâmico
+import React from "react";
+import { View, Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+export default function GameOverview() {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#1E0F3A",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text
+                style={{
+                    color: "#fff",
+                    fontSize: 28,
+                    fontWeight: "bold",
+                }}
+            >
+                Jogo: {id}
+            </Text>
+        </View>
+    );
+}

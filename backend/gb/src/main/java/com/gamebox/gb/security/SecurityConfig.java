fa15d7ad05d3edf2591c81d07cb755d321cfdc1b
PedroXits <pedroo.asn@gmail.com>
2026-05-13 14:44:53 -0300

    Criçào das configurações no SecurityConfig

--- backend/gb/src/main/java/com/gamebox/gb/security/SecurityConfig.java
+++ backend/gb/src/main/java/com/gamebox/gb/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.gamebox.gb.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/games/**").authenticated()
+                        .requestMatchers("/profile/**").authenticated()
+                        .requestMatchers("/reviews/***").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }

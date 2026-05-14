87176ec8d5bce30152824c5a456d9239743bfa06
wkarina <karii46y@gmail.com>
2026-05-13 21:37:53 -0300

    desabilitar autenticação do _layout temporariamente

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,11 +1,9 @@
 // organiza a navegação
-import React, { useContext } from "react";
-import { Stack, Redirect } from "expo-router";
+import React from "react";
+import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
-
 import { GamesProvider } from "@/context/GamesContext";
 import { AdminGamesProvider } from "@/context/AdminGamesContext";
-import { AuthContext } from "@/contexts/AuthContext";
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -14,26 +12,16 @@ export default function Layout() {
     LeagueSpartanBold: require("../assets/fonts/LeagueSpartan-Bold.ttf"),
     GeistMonoBlackItalic: require("../assets/fonts/GeistMono-BlackItalic.ttf"),
   });
-
-  // pega estado de autenticação global
-  const { signed, loading } = useContext(AuthContext);
-
-  // enquanto carrega fontes ou auth
-  if (!fontsLoaded || loading) {
+  
+  //enquanto carrega
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
     <AdminGamesProvider>
       <GamesProvider>
-
-        {/* controla acesso geral do app */}
-        {signed ? (
-          <Stack screenOptions={{ headerShown: false }} />
-        ) : (
-          <Redirect href="/(auth)/login" />
-        )}
-
+        <Stack screenOptions={{ headerShown: false }} />
       </GamesProvider>
     </AdminGamesProvider>
   );

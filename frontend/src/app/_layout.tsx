1962eec6c96f5a459c77911a76bcdc56edc42571
PedroXits <pedroo.asn@gmail.com>
2026-05-15 18:32:51 -0300

    Adição do AuthContext no _layout (funções / verificações de login)

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,28 +1,79 @@
 // organiza a navegação
-import React from "react";
+
+import React, { useContext } from "react";
+
 import { Stack } from "expo-router";
+
 import { useFonts } from "expo-font";
-import { GamesProvider } from "@/context/GamesContext";
-import { AdminGamesProvider } from "@/context/AdminGamesContext";
+
+import { GamesProvider }
+  from "@/context/GamesContext";
+
+import { AdminGamesProvider }
+  from "@/context/AdminGamesContext";
+
+import {
+  AuthContext,
+  AuthProvider
+} from "@/context/AuthContext";
+
+function Routes() {
+
+  const {
+    signed,
+    loading
+  } = useContext(AuthContext);
+
+  // enquanto carrega auth
+  if (loading) {
+    return null;
+  }
+
+  return (
+
+    <Stack screenOptions={{ headerShown: false }}>
+      {
+        signed ? (
+          // usuário logado
+          <Stack.Screen name="(tabs)" />
+        ) : (
+          // usuário não logado
+          <Stack.Screen name="(auth)" />
+        )
+      }
+    </Stack>
+  );
+}
 
 export default function Layout() {
+
   const [fontsLoaded] = useFonts({
-    Koulen: require("../assets/fonts/Koulen-Regular.ttf"),
-    Imprima: require("../assets/fonts/Imprima-Regular.ttf"),
-    LeagueSpartanBold: require("../assets/fonts/LeagueSpartan-Bold.ttf"),
-    GeistMonoBlackItalic: require("../assets/fonts/GeistMono-BlackItalic.ttf"),
+    Koulen:
+      require("../assets/fonts/Koulen-Regular.ttf"),
+
+    Imprima:
+      require("../assets/fonts/Imprima-Regular.ttf"),
+
+    LeagueSpartanBold:
+      require("../assets/fonts/LeagueSpartan-Bold.ttf"),
+
+    GeistMonoBlackItalic:
+      require("../assets/fonts/GeistMono-BlackItalic.ttf"),
   });
-  
-  //enquanto carrega
+
+  // enquanto carrega fontes
   if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <AdminGamesProvider>
-      <GamesProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </GamesProvider>
-    </AdminGamesProvider>
+
+    <AuthProvider>
+      <AdminGamesProvider>
+        <GamesProvider>
+          <Routes />
+        </GamesProvider>
+      </AdminGamesProvider>
+    </AuthProvider>
   );
 }

9f6bd9d751bd3708c94f8b20dc7a7f5a87f3e6ed
PedroXits <pedroo.asn@gmail.com>
2026-05-15 22:40:32 -0300

    Criação do CorsConfig

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,6 +4,8 @@ import type { InternalAxiosRequestConfig } from "axios";
 
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
+console.log("API URL:", API_URL);
+
 export const api = axios.create({
   baseURL: API_URL,
 });

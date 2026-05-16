a0df67c237b79a3c13c03266157443b448e491d5
wkarina <karii46y@gmail.com>
2026-05-15 21:46:11 -0300

    adicionar rolagem à tela de login e register

--- frontend/src/app/(auth)/register/index.tsx
+++ frontend/src/app/(auth)/register/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Pressable, Text, TextInput, View, Image } from "react-native";
+import { Pressable, Text, TextInput, View, Image, ScrollView } from "react-native";
 import { styles } from "./styles";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
@@ -34,7 +34,11 @@ export default function Register() {
   }
 
   return (
-    <View style={styles.page}>
+    <ScrollView
+      style={styles.page}
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
       <View style={styles.textContainer}>
         <Text style={styles.title}>Bem-Vindo</Text>
         <Text style={styles.p}>Faça parte do Gameboxd</Text>
@@ -112,6 +116,6 @@ export default function Register() {
           </Text>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }

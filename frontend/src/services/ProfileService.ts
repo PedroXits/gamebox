07a65d71826b1eac9e26c4405eba2f3079eb9ad1
PedroXits <pedroo.asn@gmail.com>
2026-05-18 00:34:20 -0300

    Adicionando a função de updateProfile no ProfileService.ts

--- frontend/src/services/ProfileService.ts
+++ frontend/src/services/ProfileService.ts
@@ -1,10 +1,24 @@
 import { api } from "@/services/api";
 import { ProfileResponse } from "@/models/profile/ProfileResponse";
 
+type UpdateProfileRequest = {
+  profileName?: string;
+  profilePhoto?: string | null;
+};
+
 export async function getProfileById(
   profileId: number
 ): Promise<ProfileResponse> {
   const response = await api.get(`/profiles/${profileId}`);
+
+  return response.data;
+}
+
+export async function updateProfile(
+  profileId: number,
+  data: UpdateProfileRequest
+): Promise<ProfileResponse> {
+  const response = await api.put(`/profiles/${profileId}`, data);
 
   return response.data;
 }

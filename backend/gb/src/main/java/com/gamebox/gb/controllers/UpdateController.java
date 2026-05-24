17a5e6bf5428c69b583f8e0d4dfe09b4679c2e89
PedroXits <pedroo.asn@gmail.com>
2026-05-24 13:44:21 -0300

    Criação do UploadController

---
+++ backend/gb/src/main/java/com/gamebox/gb/controllers/UpdateController.java
@@ -0,0 +1,31 @@
+package com.gamebox.gb.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/uploads")
+public class UpdateController {
+    @PostMapping("/games")
+    public ResponseEntity<String> uploadGameImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = System.currentTimeMillis() + "-" + file.getOriginalFilename();
+
+            Path path = Paths.get("uploads/games/" + fileName);
+
+            Files.createDirectories(path.getParent());
+            Files.write(path, file.getBytes());
+
+            String url = "http://localhost:8080/uploads/games/" + fileName;
+
+            return ResponseEntity.ok(url);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fazer upload da imagem.");
+        }
+    }
+}
